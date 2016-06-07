a106c2200c2cfbee2c100b3076e382733c274970
Carson.Ho <carson.ho@foxmail.com>
2016-06-07 15:23:38 +0800

    Finsh&Adap homePaget

--- app/src/main/java/com/example/carson_ho/borchintelligence/Fragment1.java
+++ app/src/main/java/com/example/carson_ho/borchintelligence/Fragment1.java
@@ -10,6 +10,10 @@ import android.view.ViewGroup;
  * Created by Carson_Ho on 16/5/23.
  */
 public class Fragment1 extends Fragment {
+
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_item1, null);
